ad3f04e05e6f43ab906469537cac1e65c59720b7
Takashi_U <takashi@uchibe.net>
2013-09-21 07:25:42 +0900

    TimeCardRowクラスの入力チェック機能を追加

--- lib/calc_working_hours/time_card_row.rb
+++ lib/calc_working_hours/time_card_row.rb
@@ -4,12 +4,28 @@ module CalcWorkingHours
 
   class TimeCardRow
     attr_reader :starting_time, :ending_time, :break_time, :working_hours
-    def initialize(starting_time, ending_time, break_time = [])
-      if valid_time_format?(starting_time) && valid_time_format?(ending_time)
-        self
-      else
+    def initialize(starting_time, ending_time, *break_time)
+      unless valid_time_format?(starting_time) && valid_time_format?(ending_time)
         raise "failure initialize (invalid time format! TimeCardRow class)"
       end
+
+      unless valid_time_order?(starting_time, ending_time)
+        raise "failure initialize (invalid time order! TimeCardRow class)"
+      end
+
+      unless break_time.empty?
+        unless valid_break_time?(break_time)
+          raise "failure initialize (invalid break time! TimeCardRow class)"
+        end
+      end
+      
+      break_time.each do |time|
+        unless valid_time_order?(time[0], time[1])
+          raise "failure initialize (invalid time order(break time)! TimeCardRow class)"
+        end
+      end
+
+      self
     end
 
     private
@@ -33,8 +49,26 @@ module CalcWorkingHours
       return flag
     end
 
-
     def valid_time_order?(first, second)
+      false
+      true if change_to_minute(first) < change_to_minute(second)
+    end
+
+    def valid_break_time?(break_time)
+      flag = false
+      break_time.each do |time|
+        if time.length == 2 && time.class == Array && valid_time_format?(time[0]) && valid_time_format?(time[1])
+          flag = true
+        else
+          return flag = false
+        end
+      end
+      return flag
+    end
+
+    def change_to_minute(str)
+      /(\d+):(\d+)/ =~ str
+      return $1.to_i * 60 + $2.to_i
     end
 
   end
--- spec/calc_working_hours/time_card_row_spec.rb
+++ spec/calc_working_hours/time_card_row_spec.rb
@@ -6,19 +6,41 @@ include CalcWorkingHours
 describe TimeCardRow do
   context "始業9:00、終業19:00、休憩11:00〜12:00、15:00〜16:00のとき" do
     before do
-      @time_card_row = TimeCardRow.new("9:00", "19:00", [["11:00", "12:00"], ["15:00", "16:00"]])
-#      puts "hogefuga"
+      @time_card_row = TimeCardRow.new("9:00", "19:00", ["11:00", "12:00"], ["15:00", "16:00"])
     end
+    
+
   end
 
-  context "適正な時間設定と、不適な設定時間を設定する" do
-    it do "始業22:00、終業28:00は適正な始業・終業時間設定（0:00〜48:00）である"
+  context "適正な時間設定と、不適な設定時間を設定したとき" do
+    it "始業22:00、終業28:00は適正な始業・終業時間設定（0:00〜48:00）なので、エラーは発生しないこと" do
       proc{ TimeCardRow.new("22:00", "28:00") }.should_not raise_error
     end
 
-    it do "始業44:00、終業50:00は不適正な始業・終業時間設定である"
+    it "始業44:00、終業50:00は不適正な始業・終業時間設定なので、エラーが発生すること" do
       proc{ TimeCardRow.new("44:00", "50:00") }.should raise_error
     end
+
+    it "始業四:00、終業5:00は不適正な時間フォーマットなので、エラーが発生すること" do
+      proc{ TimeCardRow.new("四:00", "5:00") }.should raise_error
+    end
+
+    it "始業9:00、終業19:00の時系列に誤りが無いので、エラーは発生しないこと" do 
+      proc { TimeCardRow.new("9:00", "19:00") }.should_not raise_error
+    end
+
+    it "始業18:00、終業8:00は時系列が誤っているので、エラーが発生すること" do
+      proc { TimeCardRow.new("18:00", "9:00") }.should raise_error
+    end
+
+    it "始業8:00、終業19:00、休憩12:00〜11:00は時系列が誤っているので、エラーが発生すること" do
+      proc { TimeCardRow.new("8:00", "19:00", ["12:00", "10:00"]) }.should raise_error
+    end
+
+    it "始業8:00、終業19:00、休憩12:00は休憩時間設定が誤っているので、エラーが発生すること" do
+      proc { TimeCardRow.new("8:00", "19:00", "12:00") }.should raise_error
+    end
+
   end
 
 end
