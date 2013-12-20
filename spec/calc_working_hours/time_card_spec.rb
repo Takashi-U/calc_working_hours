b784ebf4cd5ffb57b7862c099fd578731a536037
Takashi_U <takashi@uchibe.net>
2013-12-20 22:31:32 +0900

    残業の合計を算出する機能のテストを追加

--- spec/calc_working_hours/time_card_spec.rb
+++ spec/calc_working_hours/time_card_spec.rb
@@ -4,10 +4,15 @@ require 'spec_helper'
 include CalcWorkingHours
 
 describe TimeCard do
-  context "次の通りTimeCardRowが与えられている場合" do
+  context "始業9:00、終業19:00、休憩11:00〜12:00、15:00〜16:00が2日間のとき" do
     before do
-      @time_card = TimeCard.new
-#      puts "hogefuga"
+      @time_card = TimeCard.new(1,1)
+      @time_card.add_row(TimeCardRow.new("2013/5/12", "9:00", "19:00", ["11:00", "12:00"], ["15:00", "16:00"]).set_fixed_working_hours("7:00"))
+      @time_card.add_row(TimeCardRow.new("2013/5/13", "9:00", "19:00", ["11:00", "12:00"], ["15:00", "16:00"]).set_fixed_working_hours("7:00"))
+    end
+
+    it "残業時間のトータルは2:00となる" do
+      @time_card.total_over_time.should == "2:00"
     end
 
   end
